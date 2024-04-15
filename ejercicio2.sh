b42d22dc702da6cdb1c775be206069015e4b6800
Danny Fonseca <daniela.fonsecazumbado@gmail.com>
2024-04-14 21:11:32 -0600

    Modified

--- ejercicio2.sh
+++ ejercicio2.sh
@@ -2,4 +2,31 @@
 
 # Parametros
 
+# Recibe el nombre de un proceso y el comando para ejecutarlo
+process_name=$1
+command_name=$2
+
+# Funciones
+
+# Verifica si el proceso esta en ejecucion
+verificar_ejecucion() {
+
+    # Variables locales
+    # Status que se obtiene a partir del argumento, que es un ID de proceso
+    local status="$(ps -p $id -o stat=)"
+
+    # Funcionalidad
+    if [ "$status" == "Sl" ]; then
+        echo "El archivo esta en ejecucion."
+    else
+        echo "El archivo no esta en ejecucion."
+    fi
+}
+
 # Programa
+
+# Obtener el ID del proceso
+id=$(pgrep $process_name)
+
+# Llamar a funcion verificar_ejecucion
+verificar_ejecucion $id
